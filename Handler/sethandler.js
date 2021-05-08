4e883dfc980417a429187a69cdf9dfe65d762ee9
003-cod3r-007 <33608805+Daniel103@users.noreply.github.com>
2021-05-08 01:26:00 -0600

    Added more methods that manipulates the Set

--- Handler/sethandler.js
+++ Handler/sethandler.js
@@ -27,6 +27,62 @@ class RaidenSet extends Set {
         }
         return result
     }
+        /**
+     * Deletes a value you specified in the param
+     * @param value - The value you want to delete
+     * @returns {boolean}
+     */
+    delete(value) {
+        if(!value) throw new Error("Expect value=value, but got value=undefined")
+        return super.delete(value)
+    }
+    /**
+     * Maps the value that returns true from fn
+     * @returns {any[]}
+     */
+    map(fn) {
+        let iter = this.values()
+        let arr = []
+        for(let val of iter) {
+            if(fn(val)) arr.push(val)
+        }
+        return arr
+    }
+    /**
+     * Finds an element that return true from fn and return it's value
+     *
+     */
+    find(fn) {
+        for(let val of this) {
+            if(fn(val)) return val
+        }
+        return undefined
+    }
+    /**
+     * Maps all values in the set to an array
+     * @param limit - The length of the array you want. Default is 5
+     * @returns {any[]}
+     */
+    array(limit) {
+        let arr = []
+        for(let values of this) {
+            arr.push(values)
+        }
+        return limit ? arr.slice(0, limit) : arr.slice(0, 5)
+    }
+    /**
+     * Returns the overall value of the Set
+     * @returns {number}
+     */
+    get size() {
+        return super.size
+    }
+    /**
+     * Clears the Set of all values
+     */
+    clear() {
+        return super.clear()
+    }
 }
 
 module.exports = RaidenSet
